2446dfbf02b94cd780de16c978ebb74ad8a1e2c9
Arianne Lavada <ariannelavada@gmail.com>
2023-03-02 01:25:48 -0500

    added new-visualization component

---
+++ frontend/src/app/components/new-visualization/new-visualization.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewVisualizationComponent } from './new-visualization.component';
+
+describe('NewVisualizationComponent', () => {
+  let component: NewVisualizationComponent;
+  let fixture: ComponentFixture<NewVisualizationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NewVisualizationComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NewVisualizationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/components/new-visualization/new-visualization.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-visualization',
+  templateUrl: './new-visualization.component.html',
+  styleUrls: ['./new-visualization.component.scss']
+})
+export class NewVisualizationComponent {
+
+}
