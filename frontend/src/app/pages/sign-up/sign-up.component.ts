6a8fd5ff33f227c102bca3425c9f488b6112648c
Arianne Lavada <ariannelavada@gmail.com>
2023-03-05 13:21:01 -0500

    disabled sign up btn if invalid form; created necessary components for unique username validator

--- frontend/src/app/pages/sign-up/sign-up.component.ts
+++ frontend/src/app/pages/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { equalPasswordsValidator } from 'src/app/directives/equal-passwords.directive';
 
 @Component({
@@ -11,9 +12,9 @@ export class SignUpComponent {
   signUpForm: FormGroup;
   error = '';
 
-  constructor (private fb: FormBuilder) {
+  constructor (private fb: FormBuilder, private router: Router) {
     this.signUpForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(3)]],
+      username: ['', [Validators.required, Validators.minLength(3)]], // TODO: implement once uniqueUsernameValidator is working
       password: ['', [Validators.required, Validators.pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")]], // min. 8 chars, at least 1 letter, at least 1 num 
       confirmPassword: ['', [Validators.required]]
     }, {validator: equalPasswordsValidator});
@@ -23,6 +24,7 @@ export class SignUpComponent {
 
   signUp() {
     console.log(this.signUpForm);
+    this.router.navigate(['/']);
   }
 }
 
