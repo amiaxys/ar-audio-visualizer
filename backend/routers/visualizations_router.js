dd83f09d0ec8c7d9c817d583bd7f316d13b320f4
Sid <singhsiddhant2013@gmail.com>
2023-03-06 14:16:28 -0500

    Added default avatar, added swagger doc, added put route for visualizations

--- backend/routers/visualizations_router.js
+++ backend/routers/visualizations_router.js
@@ -76,6 +76,31 @@ visualizationsRouter.get("/users/:UserId/visualizations/:id/audio", isAuthentica
     res.sendFile(visualization.audio.path, { root: path.resolve() });
 });
 
+// update a visualization by id
+visualizationsRouter.put("/users/:UserId/visualizations/:id", upload.single("audio"), isAuthenticated, async (req, res) => {
+    const { id, UserId } = req.params;
+    if (req.user.id !== parseInt(UserId)) {
+        return res.status(403).json({ error: "Not authorized" });
+    }
+    const visualization = await Visualization.findOne({where: {id, UserId}});
+    if (!visualization) {
+        return res.status(404).json({ error: "Visualization not found" });
+    }
+    const { title, metadata } = req.body;
+    try {
+        visualization.title = title;
+        visualization.metadata = JSON.parse(metadata);
+        if (req.file) {
+            visualization.audio = req.file;
+        }
+        await visualization.save();
+        return res.json(visualization);
+    } catch (error) {
+        console.log(error);
+        return res.status(422).json({ error: "Invalid data" });
+    }
+});
+
 // delete a visualization by id
 visualizationsRouter.delete("/users/:UserId/visualizations/:id", isAuthenticated, async (req, res) => {
     const { id, UserId } = req.params;
