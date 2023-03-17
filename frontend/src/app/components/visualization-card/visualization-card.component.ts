2fc65a50f3b9d9be36cd717f6a6d1045a4b89ae6
Arianne Lavada <ariannelavada@gmail.com>
2023-03-17 00:45:41 -0400

    created card components; loaded all user's visualizations in visualizations page

--- frontend/src/app/components/visualization-card/visualization-card.component.ts
+++ frontend/src/app/components/visualization-card/visualization-card.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Visualization } from 'src/app/classes/visualization';
 
 @Component({
   selector: 'app-visualization-card',
   templateUrl: './visualization-card.component.html',
   styleUrls: ['./visualization-card.component.scss']
 })
-export class VisualizationCardComponent {
+export class VisualizationCardComponent implements OnInit {
+  constructor (library: FaIconLibrary) {
+    library.addIcons(faPenToSquare, faTrash);
+    
+  }
+  
+  ngOnInit(): void {}
+  
+  @Input() visualization!: Visualization;
+  @Output() edit = new EventEmitter<number>();
+  @Output() delete = new EventEmitter<number>();
 
+  editVisualization() {
+    this.edit.emit(parseInt(this.visualization.id));
+  }
+
+  deleteVisualization() {
+    this.delete.emit(parseInt(this.visualization.id));
+  }
 }
--- frontend/src/app/pages/visualizations/visualizations.component.ts
+++ frontend/src/app/pages/visualizations/visualizations.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Visualization } from 'src/app/classes/visualization';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-visualizations',
   templateUrl: './visualizations.component.html',
   styleUrls: ['./visualizations.component.scss'],
 })
-export class VisualizationsComponent {}
+export class VisualizationsComponent implements OnInit {
+  visualizations: Visualization[] = [];
+
+  constructor(private api: ApiService) {}
+  
+  ngOnInit(): void {
+    this.getUserVisualizations();
+  }
+
+  getUserVisualizations() {
+    this.api.me().subscribe({
+      next: (user) => {
+        this.api.getVisualizations(user.id, 1, 10).subscribe({
+          next: (res) => {
+            this.visualizations = res.rows;
+          }
+        })
+      },
+      error: (err) => {
+        console.log(`Auth error: ${err}`);
+      },
+    });
+  }
+
+  editVisualizations(userId: number) {
+  }
+
+  deleteVisualizations(userId: number) {
+
+  }
+}
