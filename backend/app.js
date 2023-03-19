5de3d2ce7c943255970aa657669d2f4867ac4a1c
Sid <singhsiddhant2013@gmail.com>
2023-03-19 15:12:39 -0400

    Updated routes

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,7 @@ app.use(
 
 // Routers
 app.use("/api/users", usersRouter);
-app.use("/api", visualizationsRouter);
+app.use("/api/visualizations", visualizationsRouter);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
--- backend/routers/visualizations_router.js
+++ backend/routers/visualizations_router.js
@@ -9,39 +9,29 @@ const upload = multer({ dest: "uploads/" });
 export const visualizationsRouter = Router();
 
 // get visualizations paginated for specified user
-visualizationsRouter.get(
-  "/users/:UserId/visualizations",
-  isAuthenticated,
-  async (req, res) => {
-    const { page, limit } = req.query;
-    const { UserId } = req.params;
-    if (req.user.id !== parseInt(UserId)) {
-      return res.status(403).json({ error: "Not authorized" });
-    }
-    let query = { where: { UserId } };
-    if (page && limit) {
-      query = {
-        ...query,
-        offset: (page - 1) * limit,
-        limit: parseInt(limit),
-      };
-    }
-    const visualizations = await Visualization.findAndCountAll(query);
-    return res.json(visualizations);
+visualizationsRouter.get("/", isAuthenticated, async (req, res) => {
+  const { page, limit } = req.query;
+  const UserId = req.user.id;
+  let query = { where: { UserId } };
+  if (page && limit) {
+    query = {
+      ...query,
+      offset: (page - 1) * limit,
+      limit: parseInt(limit),
+    };
   }
-);
+  const visualizations = await Visualization.findAndCountAll(query);
+  return res.json(visualizations);
+});
 
 // post a new visualization
 visualizationsRouter.post(
-  "/users/:UserId/visualizations",
+  "/",
   upload.single("audio"),
   isAuthenticated,
   async (req, res) => {
     const { title, metadata } = req.body;
-    const { UserId } = req.params;
-    if (req.user.id !== parseInt(UserId)) {
-      return res.status(403).json({ error: "Not authorized" });
-    }
+    const UserId = req.user.id;
     try {
       const visualization = await Visualization.create({
         title,
@@ -58,59 +48,45 @@ visualizationsRouter.post(
 );
 
 // get a visualization by id
-visualizationsRouter.get(
-  "/users/:UserId/visualizations/:id",
-  isAuthenticated,
-  async (req, res) => {
-    const { id, UserId } = req.params;
-    if (req.user.id !== parseInt(UserId)) {
-      return res.status(403).json({ error: "Not authorized" });
-    }
-    const visualization = await Visualization.findOne({
-      where: { id, UserId },
-    });
-    if (!visualization) {
-      return res.status(404).json({ error: "Visualization not found" });
-    }
-    return res.json(visualization);
+visualizationsRouter.get("/:id", isAuthenticated, async (req, res) => {
+  const { id } = req.params;
+  const visualization = await Visualization.findOne({
+    where: { id },
+  });
+  if (!visualization) {
+    return res.status(404).json({ error: "Visualization not found" });
   }
-);
+  return res.json(visualization);
+});
 
 // get audio for a visualization by id
-visualizationsRouter.get(
-  "/users/:UserId/visualizations/:id/audio",
-  isAuthenticated,
-  async (req, res) => {
-    const { id, UserId } = req.params;
-    if (req.user.id !== parseInt(UserId)) {
-      return res.status(403).json({ error: "Not authorized" });
-    }
-    const visualization = await Visualization.findOne({
-      where: { id, UserId },
-    });
-    if (!visualization) {
-      return res.status(404).json({ error: "Visualization not found" });
-    }
-    res.setHeader("Content-Type", visualization.audio.mimetype);
-    res.sendFile(visualization.audio.path, { root: path.resolve() });
+visualizationsRouter.get("/:id/audio", isAuthenticated, async (req, res) => {
+  const { id } = req.params;
+  const visualization = await Visualization.findOne({
+    where: { id },
+  });
+  if (!visualization) {
+    return res.status(404).json({ error: "Visualization not found" });
   }
-);
+  res.setHeader("Content-Type", visualization.audio.mimetype);
+  res.sendFile(visualization.audio.path, { root: path.resolve() });
+});
 
 // update a visualization by id
 visualizationsRouter.patch(
-  "/users/:UserId/visualizations/:id",
+  "/:id",
   upload.single("audio"),
   isAuthenticated,
   async (req, res) => {
-    const { id, UserId } = req.params;
-    if (req.user.id !== parseInt(UserId)) {
-      return res.status(403).json({ error: "Not authorized" });
-    }
+    const { id } = req.params;
     const visualization = await Visualization.findOne({
-      where: { id, UserId },
+      where: { id },
     });
     if (!visualization) {
       return res.status(404).json({ error: "Visualization not found" });
+    }
+    if (req.user.id !== visualization.UserId) {
+      return res.status(403).json({ error: "Not authorized" });
     }
     const { title, metadata } = req.body;
     try {
@@ -129,21 +105,17 @@ visualizationsRouter.patch(
 );
 
 // delete a visualization by id
-visualizationsRouter.delete(
-  "/users/:UserId/visualizations/:id",
-  isAuthenticated,
-  async (req, res) => {
-    const { id, UserId } = req.params;
-    if (req.user.id !== parseInt(UserId)) {
-      return res.status(403).json({ error: "Not authorized" });
-    }
-    const visualization = await Visualization.findOne({
-      where: { id, UserId },
-    });
-    if (!visualization) {
-      return res.status(404).json({ error: "Visualization not found" });
-    }
-    await visualization.destroy();
-    return res.json(visualization);
+visualizationsRouter.delete("/:id", isAuthenticated, async (req, res) => {
+  const { id } = req.params;
+  const visualization = await Visualization.findOne({
+    where: { id, UserId },
+  });
+  if (!visualization) {
+    return res.status(404).json({ error: "Visualization not found" });
   }
-);
+  if (req.user.id !== visualization.UserId) {
+    return res.status(403).json({ error: "Not authorized" });
+  }
+  await visualization.destroy();
+  return res.json(visualization);
+});
