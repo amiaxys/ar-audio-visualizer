cfc7973bc4335b127b4d89464033d0e6e3b694b8
amiaxys <amyuqiliang@gmail.com>
2023-03-31 18:13:49 -0400

    feat: add interactive form to change visualization during runtime

--- frontend/src/app/components/visualization/visualization.component.ts
+++ frontend/src/app/components/visualization/visualization.component.ts
@@ -1,4 +1,14 @@
-import { Component, ViewChild, ElementRef, Input } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 import { Entity } from '../../classes/entity';
 import { Visualization } from '../../classes/visualization';
 import { environment } from '../../../environments/environment';
@@ -10,7 +20,9 @@ import { environment } from '../../../environments/environment';
   styleUrls: ['./visualization.component.scss'],
 })
 export class VisualizationComponent {
+  // Visualization Variables
   @Input() visualization!: Visualization;
+  @Output() update = new EventEmitter<Visualization>();
   @Input() entityTypes!: string[];
 
   @ViewChild('audio') audioElmt!: ElementRef<HTMLAudioElement>;
@@ -28,29 +40,75 @@ export class VisualizationComponent {
   timeBufferLength: number = 0;
   timeDataArray: Uint8Array = new Uint8Array(this.timeBufferLength);
 
+  freqEntNum: number = 5;
   freqEntities: Entity[] = [];
+  timeEntities: Entity[] = [];
 
   hueRegex: RegExp = /(?<=hsl\()\d+(?=,)/g;
   //lightRegex: RegExp = /(?<=,\s?)\d+(?=%\))/g;
 
   planeColor: string = 'hsl(0, 0%, 0%)';
 
-  timeEntities: Entity[] = [];
-
   start: boolean = false;
 
-  // testing variables
-  //logTime: number = 0;
-  //test: boolean = true;
+  // Form Variables
+  @ViewChild('formContainer') formContainer!: ElementRef<HTMLDivElement>;
+  formHidden: boolean = true;
+  editVisForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder, private library: FaIconLibrary) {
+    this.editVisForm = this.fb.group({
+      timeColor: new FormControl({
+        value: '',
+        disabled: true,
+      }),
+      defaultTimeColor: [true],
+      newTimeEntities: this.fb.array([]),
+      freqColor: new FormControl({
+        value: '',
+        disabled: true,
+      }),
+      defaultFreqColor: [true],
+      newFreqEntities: this.fb.array([]),
+    });
+    this.library.addIcons(faCaretLeft, faCaretRight);
+  }
 
   ngOnInit(): void {
+    this.editVisForm.patchValue({
+      timeColor: this.visualization.metadata.time.color
+        ? this.visualization.metadata.time.color
+        : '',
+      defaultTimeColor: this.visualization.metadata.time.color ? false : true,
+      freqColor: this.visualization.metadata.freq.color
+        ? this.visualization.metadata.freq.color
+        : '',
+      defaultFreqColor: this.visualization.metadata.freq.color ? false : true,
+    });
+
+    // enable form controls if color exists
+    if (this.visualization.metadata.time.color) {
+      this.editVisForm.controls['timeColor'].enable();
+    }
+    if (this.visualization.metadata.freq.color) {
+      this.editVisForm.controls['freqColor'].enable();
+    }
+
+    // push form controls to form array
+    this.newTimeEntities.push(
+      new FormControl(this.visualization.metadata.time.entities[0])
+    );
+    for (let i = 0; i < 3; i++) {
+      this.newFreqEntities.push(
+        new FormControl(this.visualization.metadata.freq.entities[i])
+      );
+    }
+
     if (!AFRAME.components['time-entity']) {
       AFRAME.registerComponent('time-entity', {
         init: function () {},
         tick: function () {
-          // I'm not actually sure if this smooths the movement
+          // to smooth the movement
           this.el.object3D.position.lerp(this.el.object3D.position, 0.1);
         },
       });
@@ -60,6 +118,92 @@ export class VisualizationComponent {
   }
 
   ngAfterViewInit(): void {}
+
+  get newTimeEntities() {
+    return this.editVisForm.controls[
+      'newTimeEntities'
+    ] as FormArray<FormControl>;
+  }
+
+  get newFreqEntities() {
+    return this.editVisForm.controls[
+      'newFreqEntities'
+    ] as FormArray<FormControl>;
+  }
+
+  toggleTimeColor() {
+    if (this.editVisForm.value.defaultTimeColor) {
+      this.editVisForm.controls['timeColor'].disable();
+    } else {
+      this.editVisForm.controls['timeColor'].enable();
+    }
+  }
+
+  toggleFreqColor() {
+    if (this.editVisForm.value.defaultFreqColor) {
+      this.editVisForm.controls['freqColor'].disable();
+    } else {
+      this.editVisForm.controls['freqColor'].enable();
+    }
+  }
+
+  toggleForm() {
+    this.formContainer.nativeElement.classList.toggle('hide-form');
+    this.formHidden = this.formContainer.nativeElement.classList.contains('hide-form');
+  }
+
+  updateEntities() {
+    console.log(this.editVisForm.value);
+    this.visualization.metadata.time.color = this.editVisForm.value
+      .defaultTimeColor
+      ? null
+      : this.editVisForm.value.timeColor;
+    this.visualization.metadata.time.entities =
+      this.editVisForm.value.newTimeEntities;
+    this.visualization.metadata.freq.color = this.editVisForm.value
+      .defaultFreqColor
+      ? null
+      : this.editVisForm.value.freqColor;
+    this.visualization.metadata.freq.entities =
+      this.editVisForm.value.newFreqEntities;
+
+    this.updateFreqEntities();
+    this.updateTimeEntities();
+  }
+
+  updateTimeEntities() {
+    for (let i = 0; i < this.timeEntities.length; i++) {
+      if (this.visualization.metadata.time.color) {
+        this.timeEntities[i].color = this.visualization.metadata.time.color;
+      } else if (!this.timeEntities[i].color.match(this.hueRegex)) {
+        this.timeEntities[i].color = `hsl(${i % 360}, 100%, ${Math.min(
+          30 + i * 2,
+          100
+        )}%)`;
+      }
+      this.timeEntities[i].type =
+        'a-' + this.visualization.metadata.time.entities[0];
+    }
+  }
+
+  updateFreqEntities() {
+    for (let i = 0; i < this.freqEntities.length; i++) {
+      if (this.visualization.metadata.freq.color) {
+        this.freqEntities[i].color = this.visualization.metadata.freq.color;
+      } else if (!this.freqEntities[i].color.match(this.hueRegex)) {
+        this.freqEntities[i].color = `hsl(${
+          i * (360 / this.freqEntNum)
+        }, 100%, 53%)`;
+      }
+      this.freqEntities[i].type =
+        'a-' + this.visualization.metadata.freq.entities[i % 3];
+    }
+  }
+
+  onSubmit() {
+    this.updateEntities();
+    this.update.emit(this.visualization);
+  }
 
   initializeFreqEntities(): void {
     let metaFreqTypes = this.visualization.metadata.freq.entities;
@@ -72,50 +216,23 @@ export class VisualizationComponent {
     }
 
     let nextX = -3;
-    const freqEntNum = 5;
     let col = this.visualization.metadata.freq.color;
     const defaultCol = col ? false : true;
-    for (let i = 0; i < freqEntNum; i++) {
+    for (let i = 0; i < this.freqEntNum; i++) {
       if (!col || defaultCol) {
-        col = `hsl(${i * (360 / freqEntNum)}, 100%, 53%)`;
+        col = `hsl(${i * (360 / this.freqEntNum)}, 100%, 53%)`;
       }
       const z = i * 0.5;
-      switch (i % 3) {
-        case 0:
-          this.freqEntities[i] = {
-            type: `a-${freqTypes[0]}`,
-            position: `${nextX} 0.75 ${z}`,
-            rotation: '0 45 0',
-            radius: '0.5',
-            width: '1',
-            height: '1',
-            color: col,
-          };
-          break;
-        case 1:
-          this.freqEntities[i] = {
-            type: `a-${freqTypes[1]}`,
-            position: `${nextX} 0.75 ${z}`,
-            rotation: '0 45 0',
-            radius: '0.5',
-            width: '1',
-            height: '1.25',
-            color: col,
-          };
-          break;
-        case 2:
-          this.freqEntities[i] = {
-            type: `a-${freqTypes[2]}`,
-            position: `${nextX} 1 ${z}`,
-            rotation: '0 45 0',
-            radius: '1',
-            width: '1',
-            height: '1.25',
-            color: col,
-          };
-          break;
-      }
-      nextX += 1 + (freqEntNum - i) * 0.1;
+      this.freqEntities[i] = {
+        type: `a-${freqTypes[i % 3]}`,
+        position: `${nextX} 0.75 ${z}`,
+        rotation: '0 45 0',
+        radius: '0.5',
+        width: '1',
+        height: '1',
+        color: col,
+      };
+      nextX += 1 + (this.freqEntNum - i) * 0.1;
     }
   }
 
@@ -157,6 +274,8 @@ export class VisualizationComponent {
 
   draw(): void {
     window.requestAnimationFrame(this.draw.bind(this));
+    // update entities based on form
+    this.updateEntities();
     // pause animation when audio is paused
     this.displayPlayBtn = this.audioElmt.nativeElement.paused;
     if (this.audioElmt.nativeElement.paused) return;
