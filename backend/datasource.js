98548c60d083744146e5048f91ea9e2454592a47
Sid <singhsiddhant2013@gmail.com>
2023-03-31 17:03:18 -0400

    lint: ran prettier

--- backend/datasource.js
+++ backend/datasource.js
@@ -2,7 +2,12 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.POSTGRES_DB_NAME, process.env.POSTGRES_DB_USER, process.env.POSTGRES_DB_PASSWORD, {
-  dialect: "postgres",
-  host: process.env.POSTGRES_DB_HOST,
-});
+export const sequelize = new Sequelize(
+  process.env.POSTGRES_DB_NAME,
+  process.env.POSTGRES_DB_USER,
+  process.env.POSTGRES_DB_PASSWORD,
+  {
+    dialect: "postgres",
+    host: process.env.POSTGRES_DB_HOST,
+  }
+);
