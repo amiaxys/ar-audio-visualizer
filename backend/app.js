dd2ea40ac244332a4617be2851a6b3c4c30cff08
amiaxys <amyuqiliang@gmail.com>
2023-04-04 01:56:44 -0400

    style: remove commented out CORS middleware

--- backend/app.js
+++ backend/app.js
@@ -28,27 +28,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// Additional CORS middleware
-/* app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", () => {
-    if (whitelist.indexOf(req.headers.origin) !== -1) {
-      return req.headers.origin;
-    } else {
-      return "https://audiovisualizer.live";
-    }
-  });
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-}); */
-
 app.use(sessionMiddleware);
 
 try {
